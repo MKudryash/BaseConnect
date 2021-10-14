01460aa364680f167ab6ad5fd66880a494cd631a
Julia Mamsheva <Jukudryash@gmail.com>
2021-10-14 18:46:30 +0300

    New project Books

--- app/src/main/java/com/example/baseconnect/DBHelper.java
+++ app/src/main/java/com/example/baseconnect/DBHelper.java
@@ -8,12 +8,12 @@ public class DBHelper  extends SQLiteOpenHelper{
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final String TABLE_CONTACTS = "Book";
 
-    public static final String KEY_ID = "_id";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
-
+    public static final String KEY_ID = "IDN";
+    public static final String KEY_NAME = "Name";
+    public static final String KEY_Auth = "Author";
+    public static final String STL = "Style";
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -21,7 +21,7 @@ public class DBHelper  extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+                + " integer primary key," + KEY_NAME + " text," + KEY_Auth + " text," + STL + " text" + ")");
 
     }
 
--- app/src/main/java/com/example/baseconnect/MainActivity.java
+++ app/src/main/java/com/example/baseconnect/MainActivity.java
@@ -13,81 +13,153 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
-    public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-        Button btnAdd, btnRead, btnClear;
-        EditText etName, etEmail;
 
-        DBHelper dbHelper;
+    Button btnAdd, btnClear;
+    EditText etName, etAuthor, etStyle;
+    DBHelper dbHelper;
+    SQLiteDatabase database;
+    ContentValues contentValues;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
 
-            btnAdd = (Button) findViewById(R.id.btnAdd);
-            btnAdd.setOnClickListener(this);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
 
-            btnRead = (Button) findViewById(R.id.btnRead);
-            btnRead.setOnClickListener(this);
+        etName = (EditText) findViewById(R.id.etName);
+        etAuthor = (EditText) findViewById(R.id.TextAuthor);
+        etStyle = (EditText) findViewById(R.id.TextStyle);
 
-            btnClear = (Button) findViewById(R.id.btnClear);
-            btnClear.setOnClickListener(this);
+        dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        UpdateTable();
 
-            etName = (EditText) findViewById(R.id.etName);
-            etEmail = (EditText) findViewById(R.id.etEmail);
+    }
+    public  void UpdateTable(){
+        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
 
-            dbHelper = new DBHelper(this);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int AutIndex = cursor.getColumnIndex(DBHelper.KEY_Auth);
+            int StlIndex = cursor.getColumnIndex(DBHelper.STL);
+            TableLayout dbOutPut = findViewById(R.id.dbOutPut);
+            dbOutPut.removeAllViews();
+            do {
+                TableRow dbOuyPutRow = new TableRow( this);
+                dbOuyPutRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+                LinearLayout.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
+
+                TextView OutPutID = new TextView(this);
+                params.weight = 1.0f;
+                OutPutID.setLayoutParams(params);
+                OutPutID.setText(cursor.getString(idIndex));
+                dbOuyPutRow.addView(OutPutID);
+
+                TextView OutPutName = new TextView(this);
+                params.weight = 3.0f;
+                OutPutName.setLayoutParams(params);
+                OutPutName.setText(cursor.getString(nameIndex));
+                dbOuyPutRow.addView(OutPutName);
+
+                TextView OutPutAut = new TextView(this);
+                params.weight = 3.0f;
+                OutPutAut.setLayoutParams(params);
+                OutPutAut.setText(cursor.getString(AutIndex));
+                dbOuyPutRow.addView(OutPutAut);
+
+                TextView OutPutStl = new TextView(this);
+                params.weight = 2.0f;
+                OutPutStl.setLayoutParams(params);
+                OutPutStl.setText(cursor.getString(StlIndex));
+                dbOuyPutRow.addView(OutPutStl);
+
+                Button deleteBtn = new Button(this);
+                deleteBtn.setOnClickListener(this);
+                params.weight = 1.0f;
+                deleteBtn.setLayoutParams(params);
+                deleteBtn.setText("Удалить запись");
+                deleteBtn.setId(cursor.getInt(idIndex));
+                dbOuyPutRow.addView(deleteBtn);
+
+                dbOutPut.addView(dbOuyPutRow);
+            } while (cursor.moveToNext());
         }
+        cursor.close();
+    }
+    @Override
+    public void onClick(View v) {
 
-        @SuppressLint("NonConstantResourceId")
-        @Override
-        public void onClick(View v) {
-
-            String name = etName.getText().toString();
-            String email = etEmail.getText().toString();
-
-            SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-            ContentValues contentValues = new ContentValues();
-
-
-            switch (v.getId()) {
-
-                case R.id.btnAdd:
-                    contentValues.put(DBHelper.KEY_NAME, name);
-                    contentValues.put(DBHelper.KEY_MAIL, email);
-
-                    database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                    break;
-
-                case R.id.btnRead:
-                    Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-
-                    if (cursor.moveToFirst()) {
-                        int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                        int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                        int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                        do {
-                            Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                    ", name = " + cursor.getString(nameIndex) +
-                                    ", email = " + cursor.getString(emailIndex));
-                        } while (cursor.moveToNext());
-                    } else
-                        Log.d("mLog","0 rows");
-
-                    cursor.close();
-                    break;
-
-                case R.id.btnClear:
-                    database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                    break;
-            }
-            dbHelper.close();
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                String name = etName.getText().toString();
+                String author = etAuthor.getText().toString();
+                String style = etStyle.getText().toString();
+                contentValues = new ContentValues();
+                contentValues.put(DBHelper.KEY_NAME, name);
+                contentValues.put(DBHelper.KEY_Auth, author);
+                contentValues.put(DBHelper.STL, style);
+                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                etName.setText(null);
+                etAuthor.setText(null);
+                etStyle.setText(null);
+                UpdateTable();
+                break;
+            case R.id.btnClear:
+                database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                TableLayout dbOutPut = findViewById(R.id.dbOutPut);
+                dbOutPut.removeAllViews();
+                etName.setText(null);
+                etAuthor.setText(null);
+                etStyle.setText(null);
+                UpdateTable();
+                break;
+            default:
+                View outputDBRow = (View) v.getParent();
+                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
+                outputDB.removeView(outputDBRow);
+                outputDB.invalidate();
+                database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID+ " = ?", new String[]{String.valueOf(v.getId())});
+                contentValues = new ContentValues();
+                Cursor cursorUpdater = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                if (cursorUpdater.moveToFirst()) {
+                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_NAME);
+                    int AutIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_Auth);
+                    int StlIndex = cursorUpdater.getColumnIndex(DBHelper.STL);
+                    int realID = 1;
+                    do {
+                        if (cursorUpdater.getInt(idIndex)>realID){
+                            contentValues.put(DBHelper.KEY_ID, realID);
+                            contentValues.put(DBHelper.KEY_NAME, cursorUpdater.getString(nameIndex));
+                            contentValues.put(DBHelper.KEY_Auth, cursorUpdater.getString(AutIndex));
+                            contentValues.put(DBHelper.STL, cursorUpdater.getString(StlIndex));
+                            database.replace(DBHelper.TABLE_CONTACTS, null, contentValues);
+                        }
+                        realID++;
+                    }while (cursorUpdater.moveToNext());
+                    if (cursorUpdater.moveToLast()&& v.getId()!=realID){
+                        database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
+                    }
+                    UpdateTable();
+                }
+                break;
         }
     }
+}
 
